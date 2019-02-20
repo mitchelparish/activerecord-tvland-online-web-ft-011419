bb51f73e0eb1b481c7cbcf1bb0f9196bb617509c
Mitchel Parish <mitchelparish@gmail.com>
2019-02-20 22:36:27 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrases.rb
+++ db/migrate/006_add_catchphrases.rb
@@ -1,4 +1,4 @@
-class AddCatchphrases < ActiveRecord::Migration[4.2]
+class AddCatchphrasesToCharacters < ActiveRecord::Migration[4.2]
 
   def change
     add_column :characters, :catchphrase, :string
