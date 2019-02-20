25b9f69f54a1b08f6899cf25b0ce365437b5145b
Mitchel Parish <mitchelparish@gmail.com>
2019-02-20 23:29:11 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,8 +7,8 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    characters.map do |c|
-      "#{c.name} - #{c.show.name}"
+    self.characters.collect do |t|
+      "#{t.name} - #{t.show.name}"
     end
   end
 end
